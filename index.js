6833b91709b90b8b69171f4679f1e1dfa5cd1d08
iamjanolaerts <jan.olaerts92@gmail.com>
2019-10-23 12:02:30 -0500

    Initialized websockets

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+
+const server = app.listen(process.env.port || 5000, () => {
+    console.log('now listening to port 5000');
+})
+
+app.use(express.static('public'));
+
+const io = socket(server);
+
+io.on('connection', socket => {
+    console.log('socket connection has been made', socket.id);
+
+    socket.on('chat', data => {
+        io.sockets.emit('chat', data);
+    })
+
+    socket.on('typing', data => {
+        socket.broadcast.emit('typing', data);
+    })
+})
