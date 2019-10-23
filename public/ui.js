1fd6fac48dd8aed60c9def8f9762fe0ea262cef4
iamjanolaerts <jan.olaerts92@gmail.com>
2019-10-23 18:40:25 -0500

    Nav animations done

--- public/ui.js
+++ public/ui.js
@@ -1,3 +1,4 @@
+//server
 const socket = io.connect('http://localhost:5000');
 
 const messageForm = document.querySelector('.messageForm');
@@ -25,4 +26,53 @@ socket.on('chat', data => {
 
 socket.on('typing', data => {
     typing.innerHTML = `<p><strong>${data.user} is typing</strong></p>`;
+})
+
+//nav
+const ul = document.querySelector('ul');
+const askLinks = document.querySelectorAll('.bot-ask');
+const postLinks = document.querySelectorAll('.bot-post');
+const info = document.querySelector('.info');
+
+const createInfo = (e) => {
+    info.style.display = 'block';
+    info.style.left = `${e.pageX - 220}px`;
+    info.style.top = `${e.pageY}px`;
+    console.log(info);
+    if(e.target.classList.contains('bot-ask')){
+        info.textContent = 'Click to ask chatbot a question';
+        info.style.width = '220px';
+    }
+    if(e.target.classList.contains('bot-post')){
+        info.textContent = 'Click to post to the cloud';
+        info.style.width = '180px';
+    }
+}
+
+askLinks.forEach(link => {
+    link.addEventListener('mouseenter', e => {
+        askLinks[0].style.background = 'limegreen';
+        createInfo(e);
+    })
+})
+
+askLinks.forEach(link => {
+    link.addEventListener('mouseout', e => {
+        askLinks[0].style.background = 'orange';
+        info.style.display = 'none';
+    })
+})
+
+postLinks.forEach(link => {
+    link.addEventListener('mouseenter', e => {
+        postLinks[0].style.background = 'limegreen';
+        createInfo(e);
+    })
+})
+
+postLinks.forEach(link => {
+    link.addEventListener('mouseout', e => {
+        postLinks[0].style.background = 'darkblue';
+        info.style.display = 'none';
+    })
 })
